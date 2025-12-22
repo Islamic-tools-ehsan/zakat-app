a86996ce99d01730204dc0c13292455f7c088ea6
EHSAN <course.online.ehsan@gmail.com>
2025-12-22 15:18:49 +0500

    Force build fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ADD THIS BLOCK BELOW
   typescript: {
     ignoreBuildErrors: true,
   },
-  // If you still have PWA settings, they stay here too
 };
-
 module.exports = nextConfig;
